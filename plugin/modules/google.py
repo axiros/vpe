4f0a9a6f9225e8c1086fb34fcefd4bc582dad7f3
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-02-04 23:07:37 +0100

    feat: google lookup

---
+++ plugin/modules/google.py
@@ -0,0 +1,5 @@
+def try_load(s: str, url='n.a.'):
+    """s the content of a swagger definition file"""
+    s = s.encode().decode('utf-8-sig')
+    return {'lines': url, ':here': True}
+    return s
